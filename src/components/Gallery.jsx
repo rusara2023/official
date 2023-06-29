301f28b409d0d2fd19110d9a513fe638fb70804b
Janth Yapa <34100523+janithcyapa@users.noreply.github.com>
2023-06-29 12:05:52 +0530

    gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,47 +1,60 @@
-import React from 'react'
+import React from "react";
 import { Gallery } from "react-grid-gallery";
 
 const images = [
-    {
-       src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
-       
-       caption: "After Rain (Jeshu John - designerspics.com)",
-    },
-  
- 
-    {
-       src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
-       
-    },
-    {
-       src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
-       
-       caption: "After Rain (Jeshu John - designerspics.com)",
-    },
-    {
-       src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
-       
-       alt: "Boats (Jeshu John - designerspics.com)",
-    },
- 
-    {
-       src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
-       
-    },
-    {
-       src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
-       
-       caption: "After Rain (Jeshu John - designerspics.com)",
-    },
+  {
+    src: "https://ruusara.github.io/ruusara/assets/imgs/gallary-1.jpg",
+    caption: "",
+  },
+  {
+    src: "https://ruusara.github.io/ruusara/assets/imgs/gallary-5.jpg",
+    caption: "",
+  },
+  {
+    src: "https://ruusara.github.io/ruusara/assets/imgs/gallary-2.jpg",
+    caption: "",
+  },
+  {
+    src: "https://ruusara.github.io/ruusara/assets/imgs/gallary-3.jpg",
+    caption: "",
+  },
+  {
+    src: "https://ruusara.github.io/ruusara/assets/imgs/gallary-4.jpg",
+    caption: "",
+  },
+  {
+    src: "https://ruusara.github.io/ruusara/assets/imgs/gallary-6.jpg",
+    caption: "",
+  },
+  {
+    src: "https://ruusara.github.io/ruusara/assets/imgs/gallary-7.jpg",
+    caption: "",
+  },
+  {
+    src: "https://ruusara.github.io/ruusara/assets/imgs/gallary-8.jpg",
+    caption: "",
+  },
+  {
+    src: "https://ruusara.github.io/ruusara/assets/imgs/gallary-9.jpg",
+    caption: "",
+  },
+  {
+    src: "https://ruusara.github.io/ruusara/assets/imgs/gallary-10.jpg",
+    caption: "",
+  },
+  {
+    src: "https://ruusara.github.io/ruusara/assets/imgs/gallary-11.jpg",
+    caption: "",
+  },
 
- ];
+];
 
 function MyGallery() {
   return (
-    <div className='py-12 mx-auto'>
-        <Gallery images={images} enableImageSelection={false} rowHeight={240}/>
+    <div className="py-12 mx-auto">
+      <Gallery images={images} enableImageSelection={false} rowHeight={420} className="justify-center" />
     </div>
-  )
+  );
 }
 
-export default MyGallery
+export default MyGallery;
