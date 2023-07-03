d371136204d9ef1c8b54e975c5719ee44946fced
Janth Yapa <34100523+janithcyapa@users.noreply.github.com>
2023-07-03 22:16:17 +0530

    ad

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -3,19 +3,22 @@ import { Gallery } from "react-grid-gallery";
 
 const images = [
   {
-    src: "https://firebasestorage.googleapis.com/v0/b/webi-f2254.appspot.com/o/ruusara%2FWhatsApp%20Image%202023-07-01%20at%2019.40.32.jpg?alt=media&token=75d998a0-0e29-4085-a066-b05689ff9cf2",
+    src:
+      "https://firebasestorage.googleapis.com/v0/b/webi-f2254.appspot.com/o/ruusara%2FWhatsApp%20Image%202023-07-03%20at%2022.11.18.jpg?alt=media&token=9f0f104a-4cdd-428e-aea9-6e0d2fe32622",
     caption: "",
   },
   {
-    src: "https://firebasestorage.googleapis.com/v0/b/webi-f2254.appspot.com/o/ruusara%2FWhatsApp%20Image%202023-07-01%20at%2019.40.33.jpg?alt=media&token=7ef73ee2-38e5-4447-bc21-b4e24a4fe102",
+    src:
+      "https://firebasestorage.googleapis.com/v0/b/webi-f2254.appspot.com/o/ruusara%2FWhatsApp%20Image%202023-07-01%20at%2019.40.33.jpg?alt=media&token=7ef73ee2-38e5-4447-bc21-b4e24a4fe102",
     caption: "",
-  }
-
+  },
 ];
 
 function MyGallery() {
   return (
     <div className="py-12 mx-auto">
+      <SectionTitle title="Event Posts" />
+      <br />
       <Gallery images={images} enableImageSelection={false} rowHeight={420} className="justify-center" />
     </div>
   );
